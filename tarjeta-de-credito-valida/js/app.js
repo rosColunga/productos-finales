5de1d7e8a3bf0cbe4dfa9d7ea8b185210ce1a3c2
rosColunga <rosicolunga@hotmail.com>
2017-10-30 17:53:39 -0600

    Subiendo avances en ejercicio de Tarjeta válida

--- tarjeta-de-credito-valida/js/app.js
+++ tarjeta-de-credito-valida/js/app.js
@@ -1,3 +1,15 @@
-do {
-  var numTarjeta = prompt('Introduce un número de tarjeta');
-} while (!numTarjeta);
+do { //Obteniendo números de la tarjeta dentro de una variable e invirtiéndolos
+  var askNumber = prompt('Ingrese un número de tarjeta').split("").reverse().join("");
+} while (askNumber<=" ");//Si se ingresa un espacio en lugar de número se volverá a ejecutar el prompt
+console.log(askNumber);
+
+//function isValidCard (positions){
+var oddPosition = [];
+  for (var i = 0; i<=askNumber.length -1; i+=2){
+    var resultOdd = (askNumber[i]);
+    oddPosition = oddPosition + resultOdd;//Otorgando valores a var oddPosition
+  }
+console.log(oddPosition); //imprimiendo números en posiciones nones
+
+ //var pairPosition = [];
+ //var pairSpecialCase = []; (variables que falta obtener)
