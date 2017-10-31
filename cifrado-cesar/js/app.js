92f8d6f10cc635bc22c02ee2779b5d8e2f0001c2
rosColunga <rosicolunga@hotmail.com>
2017-10-31 12:28:24 -0600

    Actualizando ejercicio cifrado-cesar

--- cifrado-cesar/js/app.js
+++ cifrado-cesar/js/app.js
@@ -1,1 +1,21 @@
-alert('Yeah!')
+do { //Se repetirá hasta obtener respuesta
+  var userFrase = prompt('Ingrese una frase').split(""); //"abanico"  cipheredFrase = "hihupjv" (7lugares a la derecha)
+} while (!userFrase)//probablemente añadiendo variables que contengan mayúsculas y minúsculas por código ASCII. Se probará ejecución posteriormente
+console.log(userFrase);
+
+
+var asciiFromArray = []; //Añadiendo caractéres en su código ASCII
+
+function cipher (userFrase){
+
+  for (var i = 0; i< userFrase.length; i+=1){
+
+    var result = userFrase[i].charCodeAt();
+    asciiFromArray.push(result);
+
+  }
+  return asciiFromArray;
+}
+
+cipher (userFrase);
+console.log(asciiFromArray);//Mostrando códigos en consola
